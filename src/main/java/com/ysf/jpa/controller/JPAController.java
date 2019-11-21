884af884e5d875628fe0cad3dbde97db3a599e99
yuanshaofeng <yuanshaofeng@supersurs.com>
2019-11-21 13:56:00 +0800

    补充

--- src/main/java/com/ysf/jpa/controller/JPAController.java
+++ src/main/java/com/ysf/jpa/controller/JPAController.java
@@ -4,6 +4,8 @@ import com.ysf.jpa.jpa.SB;
 import com.ysf.jpa.repository.ISBRepository;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,50 +21,60 @@ import java.util.List;
 @RequestMapping("/sb")
 public class JPAController {
     @Autowired
-    private ISBRepository ISBRepository;
+    private ISBRepository isbRepository;
 
     //查询
     @RequestMapping(value = "/findAll", method = {RequestMethod.GET})
     @ApiOperation(value = "查询所有数据")
     @ResponseBody
     public List<SB> findAll() {
-        return ISBRepository.findAll();
+        return isbRepository.findAll();
     }
 
     @RequestMapping(value = "/findById", method = {RequestMethod.GET})
     @ApiOperation(value = "根据主键查询数据")
     @ResponseBody
-    public SB findById(@RequestParam(value = "id") String id) {
-        return ISBRepository.findById(id);
+    public SB findById(@RequestParam(value = "主键ID") String id) {
+        return isbRepository.findById(id);
     }
 
     @RequestMapping(value = "/findAllByXAndY", method = {RequestMethod.GET})
     @ApiOperation(value = "根据属性（x和y）查询数据")
     @ResponseBody
-    public List<SB> findAllByXAndY(@RequestParam(value = "x") String x, @RequestParam(value = "y") String y) {
-        return ISBRepository.findAllByXAndY(x, y);
+    public List<SB> findAllByXAndY(@RequestParam(value = "经度") String x, @RequestParam(value = "纬度") String y) {
+        return isbRepository.findAllByXAndY(x, y);
     }
 
     @RequestMapping(value = "/findAllByX", method = {RequestMethod.GET})
     @ApiOperation(value = "根据属性（x）查询数据")
     @ResponseBody
-    public List<SB> findAllByX(@RequestParam(value = "x") String x) {
-        return ISBRepository.findAllByX(x);
+    public List<SB> findAllByX(@RequestParam(value = "经度") String x) {
+        return isbRepository.findAllByX(x);
     }
 
     @RequestMapping(value = "/findAllOrderBySbTimeDesc", method = {RequestMethod.GET})
     @ApiOperation(value = "查询所有并按属性排序—逆序")
     @ResponseBody
     public List<SB> findAllOrderBySbTimeDesc() {
-        return ISBRepository.findAllByOrderBySbTimeDesc();
+        return isbRepository.findAllByOrderBySbTimeDesc();
     }
 
     @RequestMapping(value = "/findAllOrderBySbTime", method = {RequestMethod.GET})
     @ApiOperation(value = "查询所有并按属性排序—顺序")
     @ResponseBody
     public List<SB> findAllOrderBySbTime() {
-        return ISBRepository.findAllByOrderBySbTime();
+        return isbRepository.findAllByOrderBySbTime();
     }
+
+    //分页 查询
+    @RequestMapping(value = "/findAllPage", method = {RequestMethod.GET})
+    @ApiOperation(value = "分页查询所有数据")
+    @ResponseBody
+    public List<SB> findAllPage(@RequestParam(value = "页数") int page, @RequestParam(value = "条数") int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        return isbRepository.findAll(pageable).getContent();
+    }
+
 
 
 }
--- src/main/java/com/ysf/jpa/repository/ISBRepository.java
+++ src/main/java/com/ysf/jpa/repository/ISBRepository.java
@@ -1,6 +1,8 @@
 package com.ysf.jpa.repository;
 
 import com.ysf.jpa.jpa.SB;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -17,4 +19,6 @@ public interface ISBRepository extends JpaRepository<SB, Long> {
     List<SB> findAllByOrderBySbTime();
 
     List<SB> findAllByXAndY(String x, String y);
+    //分页
+    Page<SB> findAll(Pageable pageable);
 }
